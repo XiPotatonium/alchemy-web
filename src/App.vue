f93ac89e6b6a51ecd6e0fda614e098c3901d9f8a
shwu <xipotatonium@outlook.com>
2022-11-26 15:12:47 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      expand-on-hover
+      rail
+    >
+      <v-list>
+        <v-list-item
+          prepend-avatar="src/assets/logo.svg"
+          title="alchemy"
+          subtitle="Hello world!"
+        ></v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list density="compact" nav>
+        <v-list-item prepend-icon="mdi-folder" title="Records" value="myfiles"></v-list-item>
+        <v-list-item prepend-icon="mdi-email" title="Inbox" value="shared"></v-list-item>
+        <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+        <v-list-item prepend-icon="mdi-information" title="About" value="about"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <HelloWorld />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+  import HelloWorld from '@/components/HelloWorld.vue'
+</script>
